2bd0b3ecf347047b5a5e99c533f4d4e303322207
Khiisa Allan James <james21.khiisa@gmail.com>
2025-06-04 00:30:24 +0300

    Final deployment ready

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portfolio.urls')),  # This includes all your portfolio app URLs
+    path('', include('portfolio.urls')),  
 ]
 
 if settings.DEBUG:
