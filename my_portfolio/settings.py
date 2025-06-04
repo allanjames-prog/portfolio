35a11e9a938b95404eb37b2ad79cc4b280090e5e
Khiisa Allan James <james21.khiisa@gmail.com>
2025-06-04 11:02:03 +0300

    Added Education

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -214,3 +214,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': env('CLOUDINARY_API_KEY'),
+    'API_SECRET': env('CLOUDINARY_API_SECRET'),
+    'SECURE': True,
+    'PREFIX': 'portfolio'
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
